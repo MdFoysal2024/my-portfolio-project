5b429bb14b5362726a6f3133eabbe8129b1bc1ab
MdFoysal2024 <mdfoysal010199@gmail.com>
2025-03-01 07:16:24 +0600

    Added react simple typewriter

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,8 +4,17 @@ import { ImLinkedin } from 'react-icons/im';
 import { Link } from 'react-router-dom';
 import { LuSend } from 'react-icons/lu';
 import { MdDownloadForOffline } from 'react-icons/md';
+import { useTypewriter } from 'react-simple-typewriter'
 
 const Banner = () => {
+
+    const [text] = useTypewriter({
+        words: ['Developer'],
+        loop: 0
+    })
+
+
+
     return (
         <div id='banner' className=' px-6 md:px-0  lg:px-0 pt-8'>
 
@@ -14,7 +23,11 @@ const Banner = () => {
                 <div className='lg:w-1/2'>
                     <h3 className=" text-lg lg:text-3xl text-gray-600 font-medium"> Hello, I am</h3>
                     <h1 className=" text-3xl lg:text-7xl font-bold  text-red-600 py-4"> MD Foysal </h1>
-                    <h3 className=" text-xl lg:text-3xl text-gray-600 font-medium"> Frontend Developer</h3>
+
+                    <h3 className=" text-xl lg:text-3xl text-gray-600 font-medium">
+                    Frontend <span>{text}</span>
+                    </h3>
+
                     <p className="py-6 text-lg text-justify text-gray-400">
                         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                         quasi. In deleniti eaque aut repudiandae et a id nisi.Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
@@ -26,15 +39,15 @@ const Banner = () => {
                         <a className='text-gray-400' href="https://github.com/MdFoysal2024" target='_blank'><FaGithub /></a>
                     </div>
 
-<div className='flex flex-row items-center mt-4 md:flex-row gap-6'>
-                    <Link to='/resume'>
-                        <button className=" flex items-center justify-center gap-4  rounded-lg  font-bold border-2 cursor-pointer border-red-600 bg-red-600 text-white py-2 px-6 md:px-10 text-lg">My Resume< MdDownloadForOffline className='text-2xl' /></button>
-                    </Link>
+                    <div className='flex flex-row items-center mt-4 md:flex-row gap-6'>
+                        <Link to='/resume'>
+                            <button className=" flex items-center justify-center gap-4  rounded-lg  font-bold border-2 cursor-pointer border-red-600 bg-red-600 text-white py-2 px-6 md:px-10 text-lg">My Resume< MdDownloadForOffline className='text-2xl' /></button>
+                        </Link>
 
 
-                    <a className=" text-xl flex items-center justify-center gap-4 border-2 cursor-pointer border-red-600 text-white font-bold w-48   text-center py-2 rounded-lg" href="#contact">Contact Me <LuSend /></a>
-</div>
-    
+                        <a className=" text-xl flex items-center justify-center gap-4 border-2 cursor-pointer border-red-600 text-white font-bold w-48   text-center py-2 rounded-lg" href="#contact">Contact Me <LuSend /></a>
+                    </div>
+
 
                 </div>
 
